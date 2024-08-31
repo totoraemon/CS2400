2653dbe60fdc565a566e640a84fc7a9154ccb382
Joseline Ly <joseline.cly@gmail.com>
2024-08-31 04:02:30 -0700

    adding rps game

---
+++ rockPaperScissors/src/RockPaperScissors.java
@@ -0,0 +1,51 @@
+import java.util.Random;
+import java.util.Scanner;
+public class RockPaperScissors {
+    public static void main(String[] args) throws Exception {
+        String[] rps = {"rock", "paper", "scissors"};
+        String computerMove = rps[new Random().nextInt(rps.length)];
+        
+        Scanner scanner = new Scanner(System.in);
+        String playerMove; 
+
+        while (true) {
+            System.out.println("Please enter your move (rock, paper, or scissors)");
+            playerMove = scanner.nextLine();
+            if (playerMove.equals("rock") || playerMove.equals("paper") || playerMove.equals("scissors")) {
+                break;
+            }
+            System.out.println(playerMove + " is not a valid move.");
+        }
+
+        System.out.println("Computer played: " + computerMove);
+        
+        if (playerMove.equals(computerMove)) {
+            System.out.println("The game was a tie!");
+        }
+        else if (playerMove.equals("rock")) {
+            if (computerMove.equals("paper")) {
+                System.out.println("You lose!");
+            }
+            else {
+                System.out.println("You win!");
+            }
+        }
+        else if (playerMove.equals("paper")) {
+            if (computerMove.equals("scissors")) {
+                System.out.println("You lose!");
+            }
+            else {
+                System.out.println("You win!");
+            }
+        }
+        else if (playerMove.equals("scissors")) {
+            if (computerMove.equals("rock")) {
+                System.out.println("You lose!");
+            }
+            else {
+                System.out.println("You win!");
+            }
+        }
+        scanner.close();
+    }
+}
